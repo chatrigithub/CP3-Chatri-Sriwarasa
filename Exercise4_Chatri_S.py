7b4a8a9a1f5c6262a30d2cd7a3e090641919a7c8
chatrigithub <lookmu3t@gmail.com>
2022-01-27 15:40:56 +0700

    Exercise4_Chatri_S

---
+++ Exercise4_Chatri_S.py
@@ -0,0 +1,14 @@
+  
+from click import password_option
+
+
+Score_Foundation_Eng = 57.5
+Score_General_Bus = 84
+Score_Intro_com_sys = 36
+Score_Com_Pro = 46.7
+
+print("--- Your Score ---\n ")
+print("วิชา Foundation English : คะแนนที่ได้",Score_Foundation_Eng)
+print("วิชา General Business : คะแนนที่ได้",Score_General_Bus)
+print("วิชา Introduction to Computer Systems : คะแนนที่ได้",Score_Intro_com_sys)
+print("วิชา Computer Programming : คะแนนที่ได้",Score_Com_Pro)
